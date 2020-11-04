0abd9e2b851ce72c3df7bb608a9b1d0d4bfeafe8
Risyandi <Risyandi.94@gmail.com>
2020-11-04 17:36:56 +0700

    adding sample get API from algolia

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
-import {serve} from "https://deno.land/std@0.76.0/http/server.ts";
-const s = serve({port: 8000})
+/**
+ * example permission in deno using API
+ * and running in serve deno : deno run --allow-net index.js
+ *  */ 
 
-console.log("http://localhost:8000");
+import {serve} from "https://deno.land/std@0.76.0/http/server.ts"; // Deno serve
+const url = 'http://hn.algolia.com/api/v1/search?query=javascript'; // Algolia API
+const s = serve({ port: 8000 }) // running on port 8000
 
-for await (const req of s){
-    req.respond({ body: "hello world!"});
+for await (const req of s) {
+    const result = await fetch(url).then((result) => result.json());
+    const stories = result.hits.map((hit) => ({
+        title: hit.title,
+        url: hit.url,
+        createAt: hit.created_at_i,
+        author: hit.author,
+        totalComment: hit.num_comments,
+    }));
+
+    req.respond({
+        body: JSON.stringify(stories),
+    });
+
 }
