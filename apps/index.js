81cd7a7900d56f01e60a3ed1fe45fbd796258ebf
Risyandi <risyandi.94@gmail.com>
2023-05-31 14:18:23 +0700

    fix: update env

--- apps/index.js
+++ apps/index.js
@@ -6,10 +6,14 @@
 import {serve} from "https://deno.land/std@0.76.0/http/server.ts"; // Deno serve
 import {format} from "https://deno.land/x/date_fns/index.js"; // import library dateformat
 import {config} from "https://deno.land/x/dotenv/mod.ts"; // import library dotenv
-const url = 'http://hn.algolia.com/api/v1/search?query=javascript'; // Algolia API
+
+const url = config()['ALGOLIA_API']; // Algolia API url from file .env
 const server = serve({
+    // running on port setup in file .env
     port: parseInt(config()['PORT']),
-}); // running on port 8000
+}); 
+
+console.log(`running on: http://localhost:${config()['PORT']}/`);
 
 for await (const req of server) {
     const result = await fetch(url).then((result) => result.json());
