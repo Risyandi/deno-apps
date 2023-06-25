9a40d7f54231eb45d9783a48b886432cd9245593
Risyandi <risyandi.94@gmail.com>
2023-06-25 14:48:19 +0700

    feat: adding header every path

--- fresh-webapp/routes/[name].tsx
+++ fresh-webapp/routes/[name].tsx
@@ -1,5 +1,13 @@
 import { PageProps } from "$fresh/server.ts";
+import Header from "../components/Header.tsx";
 
 export default function Greet(props: PageProps) {
-  return <div>Hello {props.params.name}</div>;
+  return (
+    <>
+    <div class="p-4 mx-auto max-w-screen-md">
+      <Header/>
+      <div>Hello {props.params.name}</div>
+    </div>
+    </>
+  );
 }
